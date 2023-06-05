1b7378e482b2e176435fe1f247bbb96cafd6375e
JungKangWoo <todendvv91@gmail.com>
2023-06-05 16:32:09 +0900

    todayUpdate

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,12 +1,74 @@
-import { Box, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Input,
+  Radio,
+  RadioGroup,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import Layout from "./Layout";
+import { useState } from "react";
 
 export default function Contact() {
+  const name = useState("");
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log("function");
+  };
   return (
     <Layout>
-      <Box py={"140px"}>
-        <Text>Contact</Text>
-      </Box>
+      <VStack
+        as={"form"}
+        py={"140px"}
+        w={"full"}
+        alignItems={"center"}
+        px={4}
+        spacing={6}
+        bg={"#111"}
+        color={"white"}
+      >
+        <Text fontWeight={700} fontSize={20} alignItems={"center"}>
+          Application for free consultation
+        </Text>
+        <VStack w={"full"} alignItems={"flex-start"}>
+          <Text fontWeight={600}>Name</Text>
+          <Input
+            onChange={handleChange}
+            value={name}
+            placeholder="Please enter your name"
+            bg={"#ddd"}
+            color={"#111"}
+          />
+        </VStack>
+        <VStack w={"full"} alignItems={"flex-start"}>
+          <Text fontWeight={600}>Phone Number</Text>
+          <Input
+            placeholder="Please enter your Phone Number"
+            bg={"#ddd"}
+            color={"#111"}
+          />
+        </VStack>
+        <VStack w={"full"} alignItems={"flex-start"}>
+          <Text fontWeight={600}>E-mail</Text>
+          <Input
+            placeholder="Please enter your E-mail"
+            bg={"#ddd"}
+            color={"#111"}
+          />
+        </VStack>
+        <RadioGroup>
+          <Stack direction="row">
+            <Radio value="1" checked="checked">
+              Male
+            </Radio>
+            <Radio value="2">Female</Radio>
+          </Stack>
+        </RadioGroup>
+        <Button colorScheme="#ccc" border={"1px solid #ccc"}>
+          Transferring
+        </Button>
+      </VStack>
     </Layout>
   );
 }
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,12 +1,62 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Avatar, Box, HStack, Text, VStack } from "@chakra-ui/react";
 import Layout from "./Layout";
 
 export default function Profile() {
   return (
     <Layout>
-      <Box py={"140px"}>
-        <Text>Profile Page</Text>
-      </Box>
+      <VStack
+        as={"form"}
+        bg={"black"}
+        minH={"100vh"}
+        py={"140px"}
+        w={"full"}
+        px={4}
+        spacing={6}
+      >
+        <VStack spacing={6}>
+          <Avatar
+            src="https://images.unsplash.com/photo-1509847950535-14e861e5191b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=692&q=80"
+            size={"xl"}
+            color={"black"}
+            bg={"white"}
+            name="OffSet"
+          />
+          <VStack spacing={0} color={"#eee"}>
+            <Text fontSize={20} fontWeight={600}>
+              OFFSET
+            </Text>
+            <Text fontSize={14}>Rapper, Artist</Text>
+          </VStack>
+          <HStack w={"80%"} color={"#eee"}>
+            <Box
+              border={"1px"}
+              rounded={"xl"}
+              px={"20px"}
+              py={"1"}
+              w={"140px"}
+              align={"center"}
+              fontWeight={600}
+              fontSize={14}
+              textTransform={"uppercase"}
+            >
+              Contact
+            </Box>
+            <Box
+              border={"1px"}
+              rounded={"xl"}
+              px={"20px"}
+              py={"1"}
+              w={"140px"}
+              align={"center"}
+              fontWeight={600}
+              fontSize={14}
+              textTransform={"uppercase"}
+            >
+              Follow
+            </Box>
+          </HStack>
+        </VStack>
+      </VStack>
     </Layout>
   );
 }
